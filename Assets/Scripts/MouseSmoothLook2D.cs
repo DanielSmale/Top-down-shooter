309a39876693c2fc195e58344943d03c60f26f7f
DanielSmale <43572962+DanielSmale@users.noreply.github.com>
2018-10-23 17:41:55 +0100

    Tutorial 1 complete
    Tutorials 1a-d complete

---
+++ Assets/Scripts/MouseSmoothLook2D.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseSmoothLook2D : MonoBehaviour {
+
+    public Camera theCamera;
+    public float smoothing = 5.0f;
+    public float adjustmentAngle = 0.0f;
+    void Update()
+    {
+        Vector3 target = theCamera.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 difference = target = target - transform.position;
+        difference.Normalize();
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        Quaternion newRotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, rotZ + adjustmentAngle));
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * smoothing);
+    }
+}
---
+++ Assets/Scripts/TopDownCharacterController2D.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCharacterController2D : MonoBehaviour {
+    public float speed = 5.0f;
+    Rigidbody2D rigidbody2D;
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        rigidbody2D.velocity = new Vector2(x, y) * speed;
+        rigidbody2D.angularVelocity = 0.0f;
+    }
+}
