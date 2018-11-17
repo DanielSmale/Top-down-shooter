41f3e991e5ee08415d893c8dddc4163810db0fd8
DanielSmale <43572962+DanielSmale@users.noreply.github.com>
2018-11-17 12:12:16 +0000

    Multiple changes
    Added car sprite to enemy

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,24 +8,7 @@ public class Player : MonoBehaviour {
     public static event UpdateHealth OnUpdateHealth;
 
 
-    private Animator gunAnim;
-
-    private void Start()
-    {
-        gunAnim = GetComponent<Animator>();
-    }
-
-    private void Update()
-    {
-        if (Input.GetMouseButton(0))
-        {
-            gunAnim.SetBool("isFiring", true);
-        }
-        else
-        {
-            gunAnim.SetBool("isFiring", false);
-        }
-    }
+    
 
     public void SendHealthData(int health)
     {
