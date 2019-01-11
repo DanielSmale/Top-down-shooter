723542b0237a36159733bac1803072cd8f3b295e
DanielSmale <43572962+DanielSmale@users.noreply.github.com>
2019-01-11 18:41:49 +0000

    Fixed highscores bug, added new level, added and fixed road sprite

--- Top down 2D shooter/Assets/Scripts/HighScoreUI.cs
+++ Top down 2D shooter/Assets/Scripts/HighScoreUI.cs
@@ -13,20 +13,19 @@ public class HighScoreUI : MonoBehaviour
     public Text highScoreText;
     public HighScores highScore;
     private int totalHighScores = 5;
-
     public void Start()
     {
         int score = PlayerPrefs.GetInt("Score");
-        string s = PlayerPrefs.GetString("Highscores");
-
+        string s = PlayerPrefs.GetString("HighScores");
         if (string.IsNullOrEmpty(s))
         {
             highScore = new HighScores();
             highScore.scores = new List<int>();
-        } else {
+        }
+        else
+        {
             highScore = JsonUtility.FromJson<HighScores>(s);
         }
-
         if (highScore.scores.Count < totalHighScores)
         {
             int amount = totalHighScores - highScore.scores.Count;
@@ -35,30 +34,25 @@ public class HighScoreUI : MonoBehaviour
                 highScore.scores.Add(0);
             }
         }
-
         if (score > highScore.scores[totalHighScores - 1])
         {
             highScore.scores[totalHighScores - 1] = score;
         }
-
         highScore.scores.Sort();
         highScore.scores.Reverse(0, totalHighScores);
-
         highScoreText.text = "HIGH SCORES\n";
-
         for (int i = 0; i < totalHighScores; i++)
         {
             if (highScore.scores[i] == score)
             {
                 highScoreText.text += "<color=#FF0000FF>" + (i + 1).ToString() + ". " +
-highScore.scores[i].ToString() + "</color\n";
+                highScore.scores[i].ToString() + "</color>\n";
             }
             else
             {
                 highScoreText.text += (i + 1).ToString() + ". " + highScore.scores[i].ToString() + "\n";
             }
         }
-
         string scoresJSON = JsonUtility.ToJson(highScore);
         PlayerPrefs.SetString("HighScores", scoresJSON);
     }
